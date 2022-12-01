b2d4b6c31ea07b1160be2ed4b0bd437663a8140c
Aaron Lieberman <aarnliebrmn@gmail.com>
2022-11-30 20:57:31 -0800

    boiler plate, templates, and day1 dir ready

---
+++ day1/2.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include "../utils.cpp"
+
+using namespace std;
+
+void solve() {
+
+}
+
+int main() {
+  IOBoilerPlate();
+  solve();
+}
---
+++ utils.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+typedef vector<string> vs;
+typedef vector<vs> vvs;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef pair<string, string> ps;
+
+#define pb push_back
+#define its(it) it.begin(), it.end()
+#define loop(x,n) for(int x = 0; x < n; ++x)
+
+void IOBoilerPlate() {
+  ios::sync_with_stdio(0);
+  cout.tie(0);
+  cin.tie(0);
+}
+
+vs split(const string& line) {
+  vs res;
+  istringstream iss(line);
+  for (string tok; iss >> tok;) {
+    res.pb(tok);
+  }
+  return res;
+}
+
+
+
+

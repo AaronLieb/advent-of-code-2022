046bcfc67cf4e420d85a2c1f385fb44323ae7ea8
Aaron Lieberman <aarnliebrmn@gmail.com>
2022-12-14 12:50:22 -0800

    day6 complete

--- day6/2.py
+++ day6/2.py
@@ -7,9 +7,9 @@ line = lines[0]
 prev = []
 for i, c in enumerate(line):
     prev.insert(0, c)
-    if (len(prev) > 4) :
+    if (len(prev) > 14) :
         prev.pop()
-    if len(set(prev)) == 4:
+    if len(set(prev)) == 14:
         print(i + 1)
         break
 
